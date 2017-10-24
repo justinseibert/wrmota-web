281a6894efbf7071e30d0287977f831ee5a2f514
justinseibert <justinseibert@users.noreply.github.com>
2017-10-24 00:45:30 -0700

    enables customization of endpoints

--- static/admin/js/brick-builder.js
+++ static/admin/js/brick-builder.js
@@ -3,6 +3,7 @@ var Brick = function(args){
   var page = document.getElementById('pageBrick');
   var canvas = document.getElementById('canvasBrick');
   var map = document.getElementById('canvasMap');
+  var selectButton = document.getElementById('chooseTwo');
   var action = new paper.Tool();
 
   var config = {
@@ -23,6 +24,14 @@ var Brick = function(args){
         [0.25,0.25],[4,0.25],[7.75,0.25],
         [0.25,1.75],[4,1.75],[7.75,1.75],
       ],
+      // start: [
+      //   [0,0],[2,0],[4,0],[6,0],[8,0],
+      //   [0,2],[2,2],[4,2],[6,2],[8,2],
+      // ],
+      // margin: [
+      //   [0.25,0.25],[2,0.25],[4,0.25],[6,0.25],[7.75,0.25],
+      //   [0.25,1.75],[2,1.75],[4,1.75],[6,1.75],[7.75,2],
+      // ],
       bounds: {
         x: [0.25,7.75],
         y: [0.25,1.75],
@@ -30,10 +39,12 @@ var Brick = function(args){
     },
     used: {},
     map_layers: 0,
+    edit_layer: false,
     stroke: 0.25,
     hits: {
     	segments: true,
     	stroke: true,
+      fill: true,
     	tolerance: 5
     },
     ball: null,
@@ -51,7 +62,11 @@ var Brick = function(args){
 
   args = args || {};
   user.args = {
-    segment: args.segment || 8
+    segment: args.segment || 8,
+    points: {
+      id: {},
+      selected: []
+    },
   }
 
   var util = {
@@ -208,9 +223,22 @@ var Brick = function(args){
       }
     },
     any: function(scope){
+
       util.activate('brick');
       paper.projects[0].clear();
-      var p = util.chooseTwo();
+
+      var p;
+      if (!config.edit_layer){
+        p = util.chooseTwo();
+        user.args.points.selected = [p.a,p.b];
+      } else {
+        p = {
+          a: user.args.points.selected[0],
+          b: user.args.points.selected[1],
+        }
+        config.edit_layer = false;
+        config.hits.stroke = true;
+      }
       var start = {
         a: config.points.start[p.a],
         b: config.points.start[p.b],
@@ -228,7 +256,7 @@ var Brick = function(args){
       var size = new paper.Size(canvas.width,canvas.height);
       var mask = new paper.Path.Rectangle(from, size);
       var rect = new paper.Path.Rectangle(from, size);
-      rect.fillColor = 'hsl('+util.randInt(255)+',100%,50%)';
+      rect.fillColor = 'hsl('+util.randInt(255)+',100%,75%)';
 
       var path = new paper.Path();
       path.strokeColor = 'white';
@@ -239,11 +267,12 @@ var Brick = function(args){
       path.moveTo(start.a);
       path.lineTo(margin.a);
       path.lineTo(random.a);
+      // path.lineTo(random.b);
       path.lineTo(margin.b);
       path.lineTo(start.b);
 
       var grouped = new paper.Group(mask,rect,path);
-      grouped.clipped = true;
+      // grouped.clipped = true;
     },
     map: function(){
       util.activate('brick');
@@ -256,12 +285,48 @@ var Brick = function(args){
       child.applyMatrix = false;
       child.scale(0.25);
 
-      child.position.x = 10*config.map_layers + (child.bounds.width/2) + (child.bounds.width*config.map_layers);
-      child.position.y = child.bounds.height/2;
-      console.log(child);
+      var margin = util.inch(0.0625);
+      var row = Math.ceil((config.map_layers+1)/4) -1;
+      console.log(row%2, config.map_layers);
+      child.position.x = margin*(config.map_layers%4) + (child.bounds.width*(config.map_layers%4)) + child.bounds.width*(1-row%2/2);
+      child.position.y = child.bounds.height*row + margin*row + child.bounds.height/2;
 
       config.map_layers++;
       user.draw.any();
+    },
+    points: function(){
+      if (!config.edit_layer){
+        config.edit_layer = true;
+        config.hits.stroke = false;
+        selectButton.innerHTML = 'done';
+        console.log(selectButton.innerHtml);
+
+        new paper.Layer();
+        paper.project.activeLayer.name = 'temp';
+        var from = new paper.Point(0, 0);
+        var size = new paper.Size(canvas.width,canvas.height);
+        var mask = new paper.Path.Rectangle(from, size);
+        mask.fillColor = 'rgba(255,255,255,0.8)';
+
+        var circle = [];
+        for (var i = 0; i < config.points.start.length; i++){
+          circle[i] = new paper.Path.Circle(new paper.Point(config.points.margin[i]), util.inch(0.1));
+          circle[i].fillColor = 'black';
+          circle[i].strokeColor = 'black';
+          circle[i].strokeWidth = util.inch(0.05);
+          user.args.points.id[circle[i].id] = i;
+          circle[i].name = i+'_circle_selector';
+        }
+
+        var select = user.args.points.selected;
+        for (var i = 0; i < select.length; i++){
+          circle[select[i]].fillColor = 'white';
+        }
+      } else {
+        selectButton.innerHTML = 'select';
+        paper.project.layers['temp'].remove();
+        user.draw.any();
+      }
     }
   }
 
@@ -287,6 +352,17 @@ var Brick = function(args){
         }
       } else if (result.type == 'stroke') {
         hit.segment = valid.add() ? hit.path.item.insert(result.location.index + 1, event.point) : null;
+      } else if (result.type == 'fill' && result.item.name.indexOf('circle_selector') > 0){
+        var p = user.args.points;
+        console.log(p.selected);
+        if (result.item.fillColor.equals('black')){
+          result.item.fillColor = 'white';
+          var black = p.selected[1]+'_circle_selector';
+          paper.project.layers['temp'].children[black].fillColor = 'black';
+          p.selected[1] = p.selected[0];
+          p.selected[0] = p.id[result.item.id];
+          console.log(p.selected);
+        }
       }
     }
   }
