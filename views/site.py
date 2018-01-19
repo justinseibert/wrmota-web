6ad96a3c4ad2264d16eedfefb1f2bb1ee478a24f
justinseibert <justinseibert@users.noreply.github.com>
2018-01-19 09:04:29 -0800

    adds ENVIRONMENT variable to templates, enables Google Analytics in PRODUCTION mode

--- views/site.py
+++ views/site.py
@@ -1,20 +1,30 @@
 import sqlite3
-from flask import render_template, abort
+from flask import render_template, abort, current_app
 
 from wrmota.api import forms as Forms
 from wrmota.api import data as Data
 from wrmota.views import _site
 
+TEMPLATE = {}
+
+@_site.before_request
+def check_environment():
+    if current_app.config['ENVIRONMENT'] == 'PRODUCTION':
+        TEMPLATE['analytics'] = True
+    else:
+        TEMPLATE['analytics'] = False
+
+
 @_site.route('/')
 def index():
-    email = Forms.EmailForm()
-    return render_template('site/index.html', email=email)
+    TEMPLATE['email'] = Forms.EmailForm()
+    return render_template('site/index.html', template=TEMPLATE)
 
 @_site.route('/artists')
 def artists():
     file = 'static/site/data/artists.csv'
-    artists = Data.CSVdata(file).as_dict('confirmed')
-    options = {
+    TEMPLATE['artists'] = Data.CSVdata(file).as_dict('confirmed')
+    TEMPLATE['options'] = {
         'tilt' : ['rotate1','rotate-1', '', '', '', '']
     }
-    return render_template('site/artists.html', artists=artists, options=options)
+    return render_template('site/artists.html', template=TEMPLATE)
