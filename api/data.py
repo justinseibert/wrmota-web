d4ca8dd549a47da0e5d999deb37b5f077ec1f092
justinseibert <justinseibert@users.noreply.github.com>
2018-01-19 00:22:49 -0800

    adds artist page and csv rendering api

---
+++ api/data.py
@@ -0,0 +1,20 @@
+import csv
+import os
+from flask import current_app
+
+class CSVdata:
+    def __init__(self, filename):
+        self.file = os.path.join(current_app.config['PROJECT_ROOT'], filename)
+        print(current_app.config['PROJECT_ROOT'])
+
+
+    def as_dict(self,prop):
+        print(self.file)
+        read = csv.DictReader(open(self.file, 'r'))
+        as_dict = []
+
+        for line in read:
+            if line[prop] == 'x':
+                as_dict.append(line)
+
+        return as_dict
--- views/site.py
+++ views/site.py
@@ -2,9 +2,19 @@ import sqlite3
 from flask import render_template, abort
 
 from wrmota.api import forms as Forms
+from wrmota.api import data as Data
 from wrmota.views import _site
 
 @_site.route('/')
 def index():
     email = Forms.EmailForm()
     return render_template('site/index.html', email=email)
+
+@_site.route('/artists')
+def artists():
+    file = 'static/site/data/artists.csv'
+    artists = Data.CSVdata(file).as_dict('confirmed')
+    options = {
+        'tilt' : ['rotate1','rotate-1', '', '', '', '']
+    }
+    return render_template('site/artists.html', artists=artists, options=options)
