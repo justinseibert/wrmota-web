b9b4393ac093327f94287dcbb4abbcebacefdad3
justinseibert <justinseibert@users.noreply.github.com>
2018-02-08 16:55:17 -0800

    creates generic recaptcha handler, objectifies email form functions

--- static/site/js/email.js
+++ static/site/js/email.js
@@ -1,36 +1,87 @@
-function ValidateEmail(el,e){
-  e.preventDefault();
-  var user_email = document.getElementById('UserEmail').value;
-  if (user_email != '' && user_email.indexOf('@') > -1){
+// function ValidateEmail(el,e){
+//   e.preventDefault();
+//   var user_email = document.getElementById('UserEmail').value;
+//   if (user_email != '' && user_email.indexOf('@') > -1){
+//     recaptcha_func = SubmitEmail;
+//     grecaptcha.execute();
+//   } else {
+//     alert('an email is required');
+//   }
+// }
+// function SubmitEmail(){
+//   var url = "/api/subscribe";
+//   var message = '';
+//   $('#AJAXAlert').addClass('u-hide');
+//   $.ajax({
+//       type: "POST",
+//       url: url,
+//       data: $('#EmailSubmitForm').serialize(),
+//       success: function(data) {
+//         if (data.errors){
+//           $('#AJAXAlert').addClass('failure');
+//           message = 'that didn\'t work';
+//         } else {
+//           $('#AJAXAlert').addClass('success');
+//           message = 'success! be on the lookout for emails from <i>'+data.from+'</i>';
+//         }
+//         $('#AJAXAlert').html(message);
+//         $('#AJAXAlert').addClass('success');
+//         $('#AJAXAlert').removeClass('u-hide');
+//       },
+//       error: function(data){
+//         $('#AJAXAlert').html('oops, that didn\'t work for some reason');
+//         $('#AJAXAlert').addClass('failure');
+//         $('#AJAXAlert').removeClass('u-hide');
+//       }
+//   });
+// }
+
+var EmailForm = function(args){
+  this.form = args.form || $('#EmailSubmitForm');
+  this.user_email = args.user_email || $('#UserEmail');
+  this.alert_box = args.alert_box || $('#AJAXAlert');
+  this.subscribe_url = args.subscribe_url || 'api/subscribe';
+}
+
+EmailForm.prototype.validate = function(){
+  var email = this.user_email.val();
+  if (email != '' && email.indexOf('@') > -1){
+    recaptcha_pass = this;
     grecaptcha.execute();
   } else {
     alert('an email is required');
   }
 }
-function SubmitEmail(){
-  var url = "/api/subscribe";
+
+EmailForm.prototype.submit = function(){
+  var form = this.form;
+  var url = this.subscribe_url;
+  var alertbox = this.alert_box;
   var message = '';
-  $('#AJAXAlert').addClass('u-hide');
+
+  form.addClass('u-hide');
   $.ajax({
       type: "POST",
       url: url,
-      data: $('#EmailSubmitForm').serialize(),
+      data: form.serialize(),
       success: function(data) {
         if (data.errors){
-          $('#AJAXAlert').addClass('failure');
+          alertbox.addClass('failure');
           message = 'that didn\'t work';
         } else {
-          $('#AJAXAlert').addClass('success');
+          alertbox.addClass('success');
           message = 'success! be on the lookout for emails from <i>'+data.from+'</i>';
         }
-        $('#AJAXAlert').html(message);
-        $('#AJAXAlert').addClass('success');
-        $('#AJAXAlert').removeClass('u-hide');
+        alertbox.html(message);
+        alertbox.addClass('success');
+        alertbox.removeClass('u-hide');
+        form.removeClass('u-hide');
       },
       error: function(data){
-        $('#AJAXAlert').html('oops, that didn\'t work for some reason');
-        $('#AJAXAlert').addClass('failure');
-        $('#AJAXAlert').removeClass('u-hide');
+        alertbox.html('oops, that didn\'t work for some reason');
+        alertbox.addClass('failure');
+        alertbox.removeClass('u-hide');
+        form.removeClass('u-hide');
       }
   });
 }
---
+++ static/site/js/recaptcha.js
@@ -0,0 +1,4 @@
+var recaptcha_pass = null;
+var handleRecaptcha = function(){
+  recaptcha_pass.submit();
+}
