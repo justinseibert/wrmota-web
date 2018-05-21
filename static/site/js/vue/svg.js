5af766e2fae163c145c25de39478a02f3f22bda7
justinseibert <justinseibert@users.noreply.github.com>
2018-05-21 14:11:43 -0700

    let's forget the whole axios thing happened for a sec

--- static/site/js/vue/svg.js
+++ static/site/js/vue/svg.js
@@ -7,6 +7,9 @@ Vue.component('svg-arrow-down', {
 Vue.component('svg-play', {
   template: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M128 104.3v303.4c0 6.4 6.5 10.4 11.7 7.2l240.5-151.7c5.1-3.2 5.1-11.1 0-14.3L139.7 97.2c-5.2-3.3-11.7.7-11.7 7.1z"/></svg>'
 });
+Vue.component('svg-pause', {
+  template: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M96 448h106.7V64H96v384zM309.3 64v384H416V64H309.3z"/></svg>'
+});
 Vue.component('svg-fastforward', {
   template: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M480 256L262.4 110v292L480 256zM32 110v292l217.6-146L32 110z"/></svg>'
 });
