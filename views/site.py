e85e6451be5a42dc43af48dbf4573ad8e42ab492
justinseibert <justinseibert@users.noreply.github.com>
2018-05-21 20:42:23 -0700

    adds credit page and some state transitions

--- views/site.py
+++ views/site.py
@@ -26,22 +26,22 @@ def map():
     TEMPLATE['mapPage'] = 1
     return render_template('site/map.html', template=TEMPLATE)
 
-@_site.route('/login')
-def admin_login():
-    return redirect(url_for('_admin.login'))
-
-@_site.route('/lookup')
-def admin_lookup():
-    return redirect(url_for('_admin.code_lookup_for_emails'))
-
 @_site.route('/artists')
 def artists():
     artists = Database.get_artists_involved()
     TEMPLATE['artists'] = {
-        'visitor' : Database.get_dict_of(artists['visitor'], name='visitor'),
-        'local' : Database.get_dict_of(artists['local'], name='local'),
+    'visitor' : Database.get_dict_of(artists['visitor'], name='visitor'),
+    'local' : Database.get_dict_of(artists['local'], name='local'),
     }
     TEMPLATE['options'] = {
-        'tilt' : ['rotate1','rotate-1', '', '', '', '']
+    'tilt' : ['rotate1','rotate-1', '', '', '', '']
     }
     return render_template('site/artists.html', template=TEMPLATE)
+
+@_site.route('/credits')
+def credits():
+    return render_template('site/credits.html', template=TEMPLATE)
+
+@_site.route('/login')
+def admin_login():
+    return redirect(url_for('_admin.login'))
